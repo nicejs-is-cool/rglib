017a3302faeda2d12cc16588142cd54f8c6b73d7
nicejs-is-cool <xbgxtmp+nicejsverif@gmail.com>
2023-01-12 18:53:44 -0300

    rename everything to .lua and add estrings doc

--- bundle.bat
+++ bundle.bat
@@ -1,6 +1,6 @@
 rem requires luabundler and node.js to be installed
 rem npm install --location=global luabundler
-luabundler bundle src/main.luau^
- -p "%CD%/src/?.luau"^
+luabundler bundle src/main.lua^
+ -p "%CD%/src/?.lua"^
  --lua=5.1^
  -o rglib.lua
---
+++ docs/estrings.md
@@ -0,0 +1,5 @@
+# estrings
+Extended `string`s
+### API
+- `estrings.split(str, delimiter)`
+Split `str` every `delimiter`
